6103a57eee5d0586cb34e4f230ea98d2cf5ef981
Dan Wendorf <dwendorf@pivotal.io>
2016-12-23 08:57:01 -0800

    Inline set-pipeline script

--- bash_it/pcf-bosh.bash
+++ bash_it/pcf-bosh.bash
@@ -3,6 +3,7 @@ alias fwl="fw login -n system-team-pcf-bosh-pcf-bosh-1-2688"
 
 function sp(){
   pushd "$HOME/workspace/ci" > /dev/null
-    ./set-pipeline.sh
+    lpass sync
+    fly -t wings sp -p pcf-bosh -c pipelines/pcf-bosh.yml -l <(lpass show --notes 5986431050471091932) --var env_name=ol-smokey
   popd > /dev/null
 }
