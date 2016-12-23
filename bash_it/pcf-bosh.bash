2b1594d432ab868925cfb841ab1dad24bd1df569
Dan Wendorf <dwendorf@pivotal.io>
2016-12-23 09:52:32 -0800

    Add set_to_tag_filter_to_lock_cf_deployment variable for set pipeline function

--- bash_it/pcf-bosh.bash
+++ bash_it/pcf-bosh.bash
@@ -4,6 +4,8 @@ alias fwl="fw login -n system-team-pcf-bosh-pcf-bosh-1-2688"
 function sp(){
   pushd "$HOME/workspace/ci" > /dev/null
     lpass sync
-    fly -t wings sp -p pcf-bosh -c pipelines/pcf-bosh.yml -l <(lpass show --notes 5986431050471091932) --var env_name=ol-smokey
+    fly -t wings sp -p pcf-bosh -c pipelines/pcf-bosh.yml -l <(lpass show --notes 5986431050471091932) \
+        --var env_name=ol-smokey \
+        --var set_to_tag_filter_to_lock_cf_deployment=tag_filter
   popd > /dev/null
 }
