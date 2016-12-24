a0c00e96dd0e62821fe262e1eae0bf8079080382
Dan Wendorf <dwendorf@pivotal.io>
2016-12-23 18:00:11 -0800

    Add helpers for bosh targetting our environments

--- bash_it/pcf-bosh.bash
+++ bash_it/pcf-bosh.bash
@@ -14,3 +14,26 @@ function sp(){
         --var set_to_tag_filter_to_lock_cf_deployment=ignoreme
   popd > /dev/null
 }
+
+function bosh_with_env() {
+    env_name="$1"
+
+    shift
+
+    creds_path="$HOME/gcs/pcf-bosh-ci/\"$env_name\"-bosh-creds.yml"
+    creds_json="$("$HOME/workspace/ci/scripts/yaml2json" "$creds_path")"
+    uaa_client_secret="$(echo "$creds_json" | jq -r .ci_secret)"
+
+    ca_cert_file="$(mktemp)"
+    ca_cert_contents="$(echo "$creds_json" | jq -r .director_ssl.ca > "$ca_cert_file")"
+
+    bosh -e director.$env_name.gcp.pcf-bosh.cf-app.com --uaa-client=ci --uaa-client-secret=$uaa_client_secret --ca-cert="$ca_cert_file" $*
+}
+
+function bsmokey() {
+    bosh_with_env ol-smokey $*
+}
+
+function bmonte() {
+    bosh_with_env monte-nuovo $*
+}
